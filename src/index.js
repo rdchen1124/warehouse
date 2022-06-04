025163f1a9e5dcb03d08252daa7b0b8a630821ea
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2022-06-04 17:46:34 +0800

    Update code by enabling React.StrictMode and warning code about 'ReactDOM.render is no longer supported in React 18'.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
