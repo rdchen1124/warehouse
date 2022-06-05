3fbdacf5fa9f945175e69605a983afc66928fff4
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2022-06-05 22:37:15 +0800

    Add Navbar to App.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,9 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { Row } from 'react-bootstrap';
 import Header from "../Header";
+import Navbar from "../Navbar";
 import Home from '../../pages/Home';
 
 function App() {
@@ -12,9 +14,12 @@ function App() {
     <div>
       <Router>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Row className="m-1">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Row>
       </Router>
     </div>
   );
